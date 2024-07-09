9069daa7dca98fc819b78627cc4347c7c2c93f5b
danlev991 <augustineshi@gmail.com>
2024-07-09 02:21:19 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
 apt update
+apt install -y nano
 
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -F
-
-apt install -y nano
 
 curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 bash install-release.sh
